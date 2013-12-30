768eb0fe3ba54d3fdb1343c31d8b5a5867d06f86
egon12 <egon.firman@gmail.com>
2013-12-30 23:05:24 +0700

    ok add some examples

---
+++ example/complex_form/controller.php
@@ -0,0 +1,20 @@
+<?php
+
+/* The vars */
+
+/* The Function */
+
+function login() 
+{
+
+
+}
+
+
+/* Routes */
+if (isset($_GET['name']) and isset ($_GET['password']))
+{
+
+  login();
+
+}
---
+++ example/complex_form/js/phpjquery.js
@@ -0,0 +1,1 @@
+../../../phpjquery.js
